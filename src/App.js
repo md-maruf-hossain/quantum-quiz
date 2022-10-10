0e05394c7ed8a6167400680512e44609564c7ee1
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-10-11 02:00:57 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Analysis from './components/Analysis/Analysis';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/', 
+          element: <Home></Home>
+        },
+        {
+          path: '/analysis', 
+          element: <Analysis></Analysis>
+        },
+        {
+          path: '/blog', 
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <h1 className='m-3'>this is jus a paragraph</h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
